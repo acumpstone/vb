e9e689cd590a78da7f49a2ecfb6b278cda019cb2
Alexis <58956508+acumpstone@users.noreply.github.com>
2021-03-17 01:56:38 -0230

    Upload encryption project

---
+++ encryption/encryption/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnEncrypt = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.btnQuestion = New System.Windows.Forms.Button()
+        Me.btnBackwards = New System.Windows.Forms.Button()
+        Me.btnBackwardsInfo = New System.Windows.Forms.Button()
+        Me.btnAcronymInfo = New System.Windows.Forms.Button()
+        Me.btnAcronym = New System.Windows.Forms.Button()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Agency FB", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(67, 26)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(148, 52)
+        Me.TextBox1.TabIndex = 0
+        '
+        'btnEncrypt
+        '
+        Me.btnEncrypt.Font = New System.Drawing.Font("Agency FB", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEncrypt.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnEncrypt.Location = New System.Drawing.Point(67, 164)
+        Me.btnEncrypt.Name = "btnEncrypt"
+        Me.btnEncrypt.Size = New System.Drawing.Size(99, 28)
+        Me.btnEncrypt.TabIndex = 2
+        Me.btnEncrypt.Text = "Encrypt/Decrypt"
+        Me.btnEncrypt.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Agency FB", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(67, 309)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(148, 51)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.BackColor = System.Drawing.Color.DarkRed
+        Me.TrackBar1.Location = New System.Drawing.Point(90, 98)
+        Me.TrackBar1.Maximum = 3
+        Me.TrackBar1.Minimum = -3
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(111, 45)
+        Me.TrackBar1.TabIndex = 4
+        '
+        'btnQuestion
+        '
+        Me.btnQuestion.Font = New System.Drawing.Font("Agency FB", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuestion.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnQuestion.Location = New System.Drawing.Point(195, 164)
+        Me.btnQuestion.Name = "btnQuestion"
+        Me.btnQuestion.Size = New System.Drawing.Size(20, 28)
+        Me.btnQuestion.TabIndex = 5
+        Me.btnQuestion.Text = "?"
+        Me.btnQuestion.UseVisualStyleBackColor = True
+        '
+        'btnBackwards
+        '
+        Me.btnBackwards.Font = New System.Drawing.Font("Agency FB", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBackwards.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnBackwards.Location = New System.Drawing.Point(67, 209)
+        Me.btnBackwards.Name = "btnBackwards"
+        Me.btnBackwards.Size = New System.Drawing.Size(99, 26)
+        Me.btnBackwards.TabIndex = 6
+        Me.btnBackwards.Text = "Backwards"
+        Me.btnBackwards.UseVisualStyleBackColor = True
+        '
+        'btnBackwardsInfo
+        '
+        Me.btnBackwardsInfo.Font = New System.Drawing.Font("Agency FB", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBackwardsInfo.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnBackwardsInfo.Location = New System.Drawing.Point(195, 207)
+        Me.btnBackwardsInfo.Name = "btnBackwardsInfo"
+        Me.btnBackwardsInfo.Size = New System.Drawing.Size(20, 28)
+        Me.btnBackwardsInfo.TabIndex = 7
+        Me.btnBackwardsInfo.Text = "?"
+        Me.btnBackwardsInfo.UseVisualStyleBackColor = True
+        '
+        'btnAcronymInfo
+        '
+        Me.btnAcronymInfo.Font = New System.Drawing.Font("Agency FB", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAcronymInfo.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnAcronymInfo.Location = New System.Drawing.Point(195, 253)
+        Me.btnAcronymInfo.Name = "btnAcronymInfo"
+        Me.btnAcronymInfo.Size = New System.Drawing.Size(20, 28)
+        Me.btnAcronymInfo.TabIndex = 9
+        Me.btnAcronymInfo.Text = "?"
+        Me.btnAcronymInfo.UseVisualStyleBackColor = True
+        '
+        'btnAcronym
+        '
+        Me.btnAcronym.Font = New System.Drawing.Font("Agency FB", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAcronym.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnAcronym.Location = New System.Drawing.Point(67, 255)
+        Me.btnAcronym.Name = "btnAcronym"
+        Me.btnAcronym.Size = New System.Drawing.Size(99, 26)
+        Me.btnAcronym.TabIndex = 8
+        Me.btnAcronym.Text = "Acronym"
+        Me.btnAcronym.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ClientSize = New System.Drawing.Size(284, 399)
+        Me.Controls.Add(Me.btnAcronymInfo)
+        Me.Controls.Add(Me.btnAcronym)
+        Me.Controls.Add(Me.btnBackwardsInfo)
+        Me.Controls.Add(Me.btnBackwards)
+        Me.Controls.Add(Me.btnQuestion)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.btnEncrypt)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Encryption"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents btnEncrypt As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TrackBar1 As System.Windows.Forms.TrackBar
+    Friend WithEvents btnQuestion As System.Windows.Forms.Button
+    Friend WithEvents btnBackwards As System.Windows.Forms.Button
+    Friend WithEvents btnBackwardsInfo As System.Windows.Forms.Button
+    Friend WithEvents btnAcronymInfo As System.Windows.Forms.Button
+    Friend WithEvents btnAcronym As System.Windows.Forms.Button
+
+End Class
---
+++ encryption/encryption/Form1.vb
@@ -0,0 +1,87 @@
+﻿'Alexis Cumpstone
+'December 7 2015
+
+Public Class Form1
+
+    'When the Encrypt button is clicked
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles btnEncrypt.Click
+        'Store the value of the track bar in a local variable
+        Dim selectedNum As Integer = TrackBar1.Value
+        'Reset the second text box each time the user presses the button
+        TextBox2.Text = ""
+        'Store the submitted string into another local variables
+        Dim startString As String = TextBox1.Text
+        'For each index of the string:
+        For index As Integer = 0 To startString.Length - 1 Step 1
+            'Store the unicode value of each character in the startString
+            Dim stringNum As Integer = AscW(startString.Substring(index, 1))
+            'Add the value of the track bar to stringNum
+            stringNum += selectedNum
+            'Store the corresponding character of each changed unicode value in a third local varibale
+            Dim finalString As String = ChrW(stringNum)
+            'Display the final string in the second text box
+            TextBox2.Text += finalString
+        Next
+
+    End Sub
+
+    Private Sub btnQuestion_Click(sender As System.Object, e As System.EventArgs) Handles btnQuestion.Click
+        'When the first "?" button is pressed, display the encryption/decryption instructions in a message box
+        MsgBox("To encrypt your message, type it into the first text box, select a positive number on the track bar, and press the encrypt/decrypt button. To decrypt your message, type the encrypted message in the first text box. Then, select the correpsonding negative value used to encrypt the message (if a value of 3 was used the encrypt the message, a value of -3 would be used to decrypt it). Finally, press the encrypt/decrypt button.")
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'When the form loads, start playing the mission impossible theme in the backround in a loop
+        My.Computer.Audio.Play(My.Resources.missionImpossibleTheme, AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    'When the backwards button is clicked:
+    Private Sub btnBackwards_Click(sender As System.Object, e As System.EventArgs) Handles btnBackwards.Click
+        'Reset the second text box each time the user presses the button
+        TextBox2.Text = ""
+        'Store the submitted string into a local variables
+        Dim startString As String = TextBox1.Text
+        'Set space aside for the final backwards string (this allows use of it outside the for loop)
+        Dim finalString As String = ""
+        'For each index of the string (going backwards):
+        For index As Integer = startString.Length - 1 To 0 Step -1
+            'Store the current character into a local variable
+            finalString += startString.Substring(index, 1)
+        Next
+        'Display the finalString in the second text box
+        TextBox2.Text = finalString
+    End Sub
+
+    Private Sub btnBackwardsInfo_Click(sender As System.Object, e As System.EventArgs) Handles btnBackwardsInfo.Click
+        'When the second "?" button is pressed, display the monogram instructions in a message box
+        MsgBox("Enter some text into the first text box, and press the backwards button to see your message displayed backwards.")
+    End Sub
+
+    'When the monogram button is clicked:
+    Private Sub btnMonograms_Click(sender As System.Object, e As System.EventArgs) Handles btnAcronym.Click
+        'Reset the second text box each time the user presses the button
+        TextBox2.Text = ""
+        'Store the submitted string into a local variables
+        Dim startString As String = TextBox1.Text
+        'Set space aside for the final backwards string (this allows use of it outside the for loop)
+        Dim finalString As String = ""
+        'For each index of the startString:
+        For index As Integer = 0 To startString.Length - 1 Step 1
+
+            Dim currentChar = startString.Substring(index, 1)
+            If index = 0 Then
+                finalString += currentChar
+            End If
+            If currentChar = " " Then
+                finalString += startString.Substring(index + 1, 1)
+            End If
+        Next
+        'Display the finalString in the second text box
+        TextBox2.Text = finalString.ToUpper
+    End Sub
+
+    Private Sub btnMonogramInfo_Click(sender As System.Object, e As System.EventArgs) Handles btnAcronymInfo.Click
+        'When the third "?" button is pressed, display the acronym instructions in a message box
+        MsgBox("The Acronym button turns your submitted string into a word using the first letter of each submitted word.")
+    End Sub
+End Class

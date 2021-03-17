fef93b940e4e300273da9d232ff72bd2ab35e425
Alexis <58956508+acumpstone@users.noreply.github.com>
2021-03-17 01:58:51 -0230

    Upload dice roll project

---
+++ diceRoll/dice_roll/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formDiceRoll
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(214, 127)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(161, 23)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "Roll the die!"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(37, 66)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(128, 120)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'formDiceRoll
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 262)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "formDiceRoll"
+        Me.Text = "Dice Roll"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ diceRoll/dice_roll/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class formDiceRoll
+
+
+    Dim random As Integer 'Random number for dice roller
+
+
+    Private Sub btn1_Click(sender As System.Object, e As System.EventArgs) Handles btn1.Click
+        random = Rand(1, 6)
+        Select Case random
+            Case 1
+                PictureBox1.Image = My.Resources.dice_face_1
+            Case 2
+                PictureBox1.Image = My.Resources.dice_face_2
+            Case 3
+                PictureBox1.Image = My.Resources.dice_face_3
+            Case 4
+                PictureBox1.Image = My.Resources.dice_face_4
+            Case 5
+                PictureBox1.Image = My.Resources.dice_face_5
+            Case 6
+                PictureBox1.Image = My.Resources.dice_face_6
+        End Select
+    End Sub
+
+    '===============================================
+    'Rand Return
+    'a random number in a given range.
+    '
+    'Parameters:
+    ' Low The
+    'lower bounds of the range.
+    ' High The
+    'upper bounds of the range.
+    '
+    'Returns:
+    ' Returns a random number from Low..High.
+    '===============================================
+    Public Function Rand(ByVal Low As Long, ByVal High As Long) As Long
+        Rand = Int((High - Low + 1) * Rnd()) + Low
+    End Function
+
+    'To run Rand, write Rand(1, 6)
+
+    Private Sub formDiceRoll_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Randomize()
+    End Sub
+End Class
